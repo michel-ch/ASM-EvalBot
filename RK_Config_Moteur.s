3326a5adb6d6333cc5d5ef2d6e8fb9bd265c6c83
Michel <115636552+michel-ch@users.noreply.github.com>
2024-01-10 15:13:50 +0100

    Update RK_Config_Moteur.s

--- RK_Config_Moteur.s
+++ RK_Config_Moteur.s
@@ -1,7 +1,7 @@
 	;; RK - Evalbot (Cortex M3 de Texas Instrument); 
 ; programme - Pilotage 2 Moteurs Evalbot par PWM tout en ASM (configure les pwms + GPIO)
 
-;Les pages se réfèrent au datasheet lm3s9b92.pdf
+;Les pages se rÃ©fÃ¨rent au datasheet lm3s9b92.pdf
 
 ;Cablage :
 ;pin 10/PD0/PWM0 => input PWM du pont en H DRV8801RT
@@ -59,7 +59,7 @@ PWM1GENA		EQU		PWM_BASE+0x0A0
 PWM1GENB		EQU		PWM_BASE+0x0A4
 
 
-VITESSE			EQU		0x1A2	; Valeures plus petites => Vitesse plus rapide exemple 0x192
+VITESSE			EQU		0x152	; Valeures plus petites => Vitesse plus rapide exemple 0x192
 								; Valeures plus grandes => Vitesse moins rapide exemple 0x1B2
 						
 						
@@ -134,7 +134,7 @@ MOTEUR_INIT
         ORR	r0, r0, #0x01 ;
         str r0, [r6]
 	
-	;;-----------PWM0 pour moteur 1 connecté à PD0
+	;;-----------PWM0 pour moteur 1 connectÃ© Ã  PD0
 	;;PWM0 produit PWM0 et PWM1 output
 	;;Config Modes PWM0 + mode GenA + mode GenB
 		ldr r6, = PWM0CTL
@@ -171,7 +171,7 @@ MOTEUR_INIT
 		ORR	r0,	r0,	#0x07
 		str	r0,	[r6]
 
-	;;-----------PWM2 pour moteur 2 connecté à PH0
+	;;-----------PWM2 pour moteur 2 connectÃ© Ã  PH0
 	;;PWM1block produit PWM2 et PWM3 output
 		;;Config Modes PWM2 + mode GenA + mode GenB
 		ldr r6, = PWM1CTL
@@ -227,7 +227,7 @@ MOTEUR_INIT
 		ldr	r0, [r6]
 		ORR	r0,	#(GPIO_0+GPIO_1+GPIO_2+GPIO_5)	
 		str	r0,[r6]	
-	;Port D : mise à 1 de slow Decay et 12V et mise à 0 pour dir et pwm
+	;Port D : mise Ã  1 de slow Decay et 12V et mise Ã  0 pour dir et pwm
 		ldr	r6, =(GPIODATA_D+((GPIO_0+GPIO_1+GPIO_2+GPIO_5)<<2)) 
 		mov	r0, #(GPIO_2+GPIO_5) ; #0x24
 		str	r0,[r6]
@@ -244,7 +244,7 @@ MOTEUR_INIT
 		ldr	r6, =GPIODEN_H
 		mov r0, #0x03	
 		str	r0,[r6]	
-	;Port H : mise à 1 pour dir 
+	;Port H : mise Ã  1 pour dir 
 		ldr	r6, =(GPIODATA_H +(GPIO_1<<2))
 		mov	r0, #0x02
 		str	r0,[r6]		
@@ -258,28 +258,28 @@ MOTEUR_DROIT_ON
 		;Enable sortie PWM0 (bit 0), p1145 
 		ldr	r6,	=PWMENABLE
 		ldr r0, [r6]
-		orr r0,	#0x01 ;bit 0 à 1
+		orr r0,	#0x01 ;bit 0 Ã  1
 		str	r0,	[r6]
 		BX	LR
 
 MOTEUR_DROIT_OFF 
 		ldr	r6,	=PWMENABLE
 		ldr r0,	[r6]
-		and	r0,	#0x0E	;bit 0 à 0
+		and	r0,	#0x0E	;bit 0 Ã  0
 		str	r0,	[r6]
 		BX	LR
 
 MOTEUR_GAUCHE_ON
 		ldr	r6,	=PWMENABLE
 		ldr	r0, [r6]
-		orr	r0,	#0x04	;bit 2 à 1
+		orr	r0,	#0x04	;bit 2 Ã  1
 		str	r0,	[r6]
 		BX	LR
 
 MOTEUR_GAUCHE_OFF
 		ldr	r6,	=PWMENABLE
 		ldr	r0,	[r6]
-		and	r0,	#0x0B	;bit 2 à 0
+		and	r0,	#0x0B	;bit 2 Ã  0
 		str	r0,	[r6]
 		BX	LR
 
